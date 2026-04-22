bdb7d198b26b3b1559a7c7e28e22df0ec27eb17f
sanatanaalana11-del <sanatanaalana11@gmail.com>
2026-04-22 20:22:45 -0300

    SEI LA

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <title>Site TDS III</title>
 </head>
 <body>
-    <?php echo "<h1>Olá, mundo!</h1>" ?>
+    <?php echo "<h1> querido sei lá oquÊ ,soube que o professor 'Diego MARTINS' FALOU MAL DE MIL E FICOU COAGINDO E ETC!</h1>" ?>
 </body>
 </html>
